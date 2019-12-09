ee3b90b85c7fc8644cfc534f6881e4beb2b92aaa
Fedor <fedykonovalov@gmail.com>
2019-12-09 18:06:46 +0300

    Updates

---
+++ precache-manifest.4c70147904efc5f20cb24de05ebc7da4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb4bf5a4bef524b0d785732d86b6d8e",
+    "url": "/Star-db/index.html"
+  },
+  {
+    "revision": "171c003acce07f81330a",
+    "url": "/Star-db/static/css/main.f7af87ee.chunk.css"
+  },
+  {
+    "revision": "3bffd3a2369f64ddf9c5",
+    "url": "/Star-db/static/js/2.e534c6d7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Star-db/static/js/2.e534c6d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "171c003acce07f81330a",
+    "url": "/Star-db/static/js/main.14ecb7c7.chunk.js"
+  },
+  {
+    "revision": "0ba653adadb5081006b2",
+    "url": "/Star-db/static/js/runtime-main.98ddc6d7.js"
+  }
+]);
