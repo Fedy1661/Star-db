181f177220f4c69780f2a555d5eedcc36bf93c18
Fedor <fedykonovalov@gmail.com>
2019-12-17 17:50:51 +0300

    beta 0.1

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,23 +3,26 @@ import { Link } from 'react-router-dom';
 import './header.css';
 
 const Header = () => {
-
+  // const loca = window.location.href;
   return (
     <div className="header d-flex">
       <h3>
-        <Link to="/">
+        <Link to='/'>
           StarDB
         </Link>
       </h3>
       <ul className="d-flex">
         <li>
-          <Link to='/people/2'>People</Link>
+          <Link to={{
+            pathname: '/people/2',
+            state: { fromDashboard: true }
+          }}>People</Link>
         </li>
         <li>
-          <Link to="/planets/2">Planets</Link>
+          <Link to='/planets/2'>Planets</Link>
         </li>
         <li>
-          <Link to="/starships/15">Starships</Link>
+          <Link to='/starships/15'>Starships</Link>
         </li>
       </ul>
     </div>
